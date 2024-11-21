83b67d22020614f04e2fe1e87cc398c079633903
Joss-Sue <166410015+Joss-Sue@users.noreply.github.com>
2024-11-21 13:49:46 -0600

    borrar 2 atras

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,10 +1,25 @@
 import cors from 'cors'
 
 export const corsMiddleware = () => cors({
-  origin: '*', // Permite todos los orígenes
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
-  credentials: true, // Si es necesario enviar cookies o credenciales
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:3000',
+      'http://localhost:1234',
+      'https://movies.com',
+      'https://midu.dev'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
 })
 
 // métodos normales: GET/HEAD/POST
